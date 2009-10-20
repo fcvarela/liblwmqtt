695f5b92f51089a3b3ad426ba9b1159d94886789
fcvarela <fcvarela@adf7b220-bcd1-11de-afc4-699a8c260df9>
2009-10-20 11:06:58 +0000

    Sends 40K different msgs to broker Test
    git-svn-id: http://liblwmqtt.googlecode.com/svn/trunk@4 adf7b220-bcd1-11de-afc4-699a8c260df9

--- test/test.c
+++ test/test.c
@@ -2,14 +2,15 @@
 #include "libmqtt.h"
 
 int main(int argc, char **argv) {
+	char msg[128];
 	mqtt_broker_handle_t broker;
 	
 	broker.port = 1883;
 	strcpy(broker.hostname, "127.0.0.1");
 	strcpy(broker.clientid, "client1");
 
-//	int result = mqtt_connect(&broker);
-//	printf("Connect: %d\n", result);
+	int result = mqtt_connect(&broker);
+	printf("Connect: %d\n", result);
 
 	//result = mqtt_subscribe(&broker, "hello/world", NULL);
 	//printf("Subscribe: %d\n", result);
@@ -18,7 +19,8 @@ int main(int argc, char **argv) {
 	for (i=0; i<40000; i++) {
 		if (i % 1000 == 0)
 			printf("Sent %d messages\n", i);
-		mqtt_publish(&broker, "hello/world", "client1 present");
+		sprintf(msg, "Message number %d", i);
+		mqtt_publish(&broker, "hello/world", msg);
 	}
 	return 0;
 }
